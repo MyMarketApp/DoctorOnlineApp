0032da82962ed1181ad6ea2193b06296dec72ff1
Gonchi <a20135298@pucp.edu.pe>
2020-06-25 12:40:35 -0700

    Home appointments redirect

--- src/screens/SpecialtyFlow/Home.js
+++ src/screens/SpecialtyFlow/Home.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component, useEffect, useState } from "react";
 import {
   StyleSheet,
   View,
@@ -7,59 +7,77 @@ import {
   TouchableHighlight,
   TouchableOpacity,
   FlatList,
-} from 'react-native';
-import Button from 'react-native-button';
-import ajax from '../../services/Routes';
-import { useFonts } from '@use-expo/font';
-import { AppLoading } from 'expo';
-import { connect } from 'react-redux';
-import { mapStateToProps, mapDispatchToProps } from '../../components/Redux';
+} from "react-native";
+import Button from "react-native-button";
+import ajax from "../../services/Routes";
+import { useFonts } from "@use-expo/font";
+import { AppLoading } from "expo";
+import { connect } from "react-redux";
+import { mapStateToProps, mapDispatchToProps } from "../../components/Redux";
 
 const Home = (props) => {
   let [fontsLoaded] = useFonts({
-    'Montserrat-Medium': require('../../../assets/fonts/Montserrat-Medium.ttf'),
-    'Montserrat-Bold': require('../../../assets/fonts/Montserrat-Bold.ttf'),
+    "Montserrat-Medium": require("../../../assets/fonts/Montserrat-Medium.ttf"),
+    "Montserrat-Bold": require("../../../assets/fonts/Montserrat-Bold.ttf"),
   });
   const { specialties } = props;
   const { appointments } = props;
   const [topdoctors, TopDoctors] = useState([]);
   const [index, setIndex] = useState();
+  const [showAppointments, ShowAppointments] = useState([]);
 
   useEffect(() => {
-    console.log('EspecialidadesMain');
+    console.log("EspecialidadesMain");
     async function retrieveTopDoctors() {
       const response = await ajax.TopDoctors();
       TopDoctors(response.body);
     }
+    function retrieveAppointments() {
+      var d = new Date();
+      let date =
+        d.getFullYear() +
+        "-" +
+        (d.getMonth() + 1 < 10
+          ? "0" + (d.getMonth() + 1).toString()
+          : (d.getMonth() + 1).toString()) +
+        "-" +
+        d.getDate();
+      ShowAppointments(appointments.filter((a) => a.date >= date));
+    }
     retrieveTopDoctors();
-  }, []);
+    retrieveAppointments();
+  }, [appointments]);
 
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
     return (
-      <View style={{ flex: 1, backgroundColor: '#F6F7FA' }}>
+      <View style={{ flex: 1, backgroundColor: "#F6F7FA" }}>
         <View style={styles.logo}>
           <Image
             style={{ width: 253, height: 34 }}
-            source={require('../../../assets/LogoHorizontal.png')}
+            source={require("../../../assets/LogoHorizontal.png")}
           />
         </View>
-        <View style={styles.alert}>
-          <Text>Usted tiene {appointments.length} citas reservadas</Text>
-        </View>
+        <TouchableOpacity
+          onPress={() => props.navigation.navigate("MyAppointmentsFlow")}
+        >
+          <View style={styles.alert}>
+            <Text>Usted tiene {showAppointments.length} citas reservadas</Text>
+          </View>
+        </TouchableOpacity>
         <View style={styles.section}>
           <View style={styles.SecHeader}>
             <Text style={styles.Sectitle}>Especialidades</Text>
             <Text
               style={styles.link}
-              onPress={() => props.navigation.navigate('Specialties')}
+              onPress={() => props.navigation.navigate("Specialties")}
             >
               Ver más
             </Text>
           </View>
 
-          <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+          <View style={{ flexDirection: "row", justifyContent: "center" }}>
             <FlatList
               data={specialties}
               horizontal={true}
@@ -67,7 +85,7 @@ const Home = (props) => {
               renderItem={({ item }) => (
                 <TouchableOpacity
                   onPress={() =>
-                    props.navigation.navigate('Specialty', {
+                    props.navigation.navigate("Specialty", {
                       specialty: item,
                     })
                   }
@@ -82,9 +100,9 @@ const Home = (props) => {
                       style={{
                         fontSize: 16,
                         marginTop: 15,
-                        alignItems: 'center',
-                        fontFamily: 'Montserrat-Bold',
-                        color: '#2F2929',
+                        alignItems: "center",
+                        fontFamily: "Montserrat-Bold",
+                        color: "#2F2929",
                       }}
                     >
                       {item.name}
@@ -104,8 +122,8 @@ const Home = (props) => {
           </View>
           <View
             style={{
-              flexDirection: 'column',
-              justifyContent: 'center',
+              flexDirection: "column",
+              justifyContent: "center",
             }}
           >
             <FlatList
@@ -115,18 +133,18 @@ const Home = (props) => {
               renderItem={({ item }) => (
                 <TouchableOpacity
                   onPress={() =>
-                    props.navigation.navigate('DoctorDetail', {
+                    props.navigation.navigate("DoctorDetail", {
                       doctor: item,
                       doctorSpec: item.specialty.name,
                     })
                   }
                 >
                   <View style={styles.topDoc}>
-                    <View style={{ flexDirection: 'row' }}>
+                    <View style={{ flexDirection: "row" }}>
                       <View
                         style={{
-                          flexDirection: 'row',
-                          justifyContent: 'flex-start',
+                          flexDirection: "row",
+                          justifyContent: "flex-start",
                         }}
                       >
                         <Image
@@ -135,10 +153,10 @@ const Home = (props) => {
                         />
                       </View>
 
-                      <View style={{ flexDirection: 'column', marginLeft: 10 }}>
+                      <View style={{ flexDirection: "column", marginLeft: 10 }}>
                         <View>
                           <Text style={styles.docNameText}>
-                            {item.idGender === 1 ? 'Dr.' : 'Dra.'} {item.name}{' '}
+                            {item.idGender === 1 ? "Dr." : "Dra."} {item.name}{" "}
                             {item.lastName}
                           </Text>
                         </View>
@@ -152,8 +170,8 @@ const Home = (props) => {
 
                     <View
                       style={{
-                        flexDirection: 'row',
-                        justifyContent: 'flex-end',
+                        flexDirection: "row",
+                        justifyContent: "flex-end",
                         marginRight: 5,
                       }}
                     >
@@ -178,11 +196,11 @@ const styles = StyleSheet.create({
     marginTop: 58,
   },
   alert: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 15,
     height: 66,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 25,
     marginHorizontal: 25,
   },
@@ -191,41 +209,41 @@ const styles = StyleSheet.create({
     marginTop: 25,
   },
   SecHeader: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 25,
   },
   Sectitle: {
-    fontFamily: 'Montserrat-Bold',
+    fontFamily: "Montserrat-Bold",
     fontSize: 20,
-    color: '#414968',
+    color: "#414968",
   },
   link: {
-    fontFamily: 'Montserrat-Medium',
+    fontFamily: "Montserrat-Medium",
     fontSize: 14,
-    color: '#828282',
+    color: "#828282",
   },
   specialityBox: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 15,
     width: 120,
     height: 160,
     padding: 10,
     marginLeft: 5,
     marginRight: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   Image: {
     width: 50,
     height: 50,
   },
   topDoc: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: '#EAECF4',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#EAECF4",
     borderRadius: 15,
     padding: 10,
     marginBottom: 15,
@@ -237,19 +255,19 @@ const styles = StyleSheet.create({
   },
   docNameText: {
     fontSize: 16,
-    fontFamily: 'Montserrat-Bold',
-    color: '#2F2929',
+    fontFamily: "Montserrat-Bold",
+    color: "#2F2929",
   },
   docSpecText: {
     fontSize: 12,
-    fontFamily: 'Montserrat-Medium',
-    color: '#4F4F4F',
+    fontFamily: "Montserrat-Medium",
+    color: "#4F4F4F",
   },
   rateText: {
     fontSize: 30,
-    fontFamily: 'Montserrat-Bold',
-    color: '#F2C94C',
-    textAlignVertical: 'center',
+    fontFamily: "Montserrat-Bold",
+    color: "#F2C94C",
+    textAlignVertical: "center",
   },
 });
 
